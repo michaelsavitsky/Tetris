73852feaeacdc4159c35c0f9e9b5a5bba75c73b2
Mike Savitsky <michael.savitsky@gmail.com>
2020-09-03 22:29:51 -0400

    Blocks now fall and stay at the bottom once fallen

--- app.js
+++ app.js
@@ -1,28 +1,75 @@
+var settledBlocks = Array.from(Array(20), () => new Array(40));
+console.log(settledBlocks)
+
 function loadGame(){
     var canvas = document.getElementById("gameCanvas");
     if (canvas.getContext){
         var ctx = canvas.getContext('2d');
+        ctx.fillRect(0, 0, 300, 600)
+        loop(ctx);
+    }
+}
+
+function loop(ctx){
+    var newBlock = new GameBlock();
+    setInterval(function() {
+        //Fill background
+        ctx.fillStyle = "black";
         ctx.fillRect(0, 0, 300, 600);
+        //Fill settled blocks
+        ctx.fillStyle = "red"
+        for (let i=0 ; i<settledBlocks.length ; i++){
+            let temp = settledBlocks[i];
+            for (let j=0 ; j<temp.length ; j++){
+                if (settledBlocks[i][j] === true){
+                    ctx.fillRect(i*15, j*15, 15, 15);
+                }
+            }
+        }
+
+        //Fill current block
+        newBlock.blocks.forEach((item, i) => {
+            item.draw(ctx)
+        });
+        //Update current block
+        newBlock.blocks.forEach((item, i) => {
+            item.y += 1;
+            if (item.y === 39){
+                //Block has hit bottom
+                newBlock.blocks.forEach((item, i) => {
+                    settledBlocks[item.x][item.y] = true
+                });
+                console.log(settledBlocks)
+                newBlock = new GameBlock();
+            }
+        });
+    }, 300);
+}
+
+class GameBlock {
+    constructor() {
+        var ran = Math.floor(Math.random() * 20);
+        this.blocks = [
+            new Block(ran, 0),
+            new Block(ran, 1),
+            new Block(ran, 2),
+            new Block(ran, 3)
+        ]
+    }
+    draw(ctx) {
+        this.blocks.forEach((item, i) => {
+            item.draw();
+        });
     }
 }
-
-function drawBlock(x, y, context, colour){
-    context.fillStyle = colour;
-    context.fillRect(x*15, y*15, 15, 15);
-}
-
 class Block {
-    constructor(x1, x2, x3, x4, y1, y2, y3, y4) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.x3 = x3;
-        this.x4 = x4;
-        this.y1 = y1;
-        this.y2 = y2;
-        this.y3 = y3;
-        this.y4 = y4;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.colour = "red";
     }
-    draw(){
-
+    draw(ctx) {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.x*15, this.y*15, 15, 15)
     }
 }
