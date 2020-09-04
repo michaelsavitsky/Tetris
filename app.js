55e3614c7b9adf81d6db5ad74a5b9e79c641e1a3
Mike Savitsky <michael.savitsky@gmail.com>
2020-09-04 15:31:11 -0400

    Added multiple types of blocks\nTouched up vertical collision

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var settledBlocks = Array.from(Array(20), () => new Array(40));
-console.log(settledBlocks)
+var newBlock = new GameBlock();
+// console.log(settledBlocks)
 
 function loadGame(){
     var canvas = document.getElementById("gameCanvas");
@@ -10,9 +11,25 @@ function loadGame(){
     }
 }
 
-function loop(ctx){
-    var newBlock = new GameBlock();
+function loop(ctx) {
     setInterval(function() {
+        newBlock.blocks.forEach((block, index) => block.y += 1)
+        let collided = false;
+        newBlock.blocks.forEach((block, index) => {
+            if (settledBlocks[block.x][block.y] || block.y === 40) {
+                collided = true;
+            }
+        });
+        if (collided) {
+            newBlock.blocks.forEach((block, i) => {
+                block.y -= 1;
+                settledBlocks[block.x][block.y] = true;
+                newBlock = new GameBlock();
+                //check for lines in settledBlocks
+                //remove lines from settled blocks
+                //move all lines above removed row down
+            });
+        }
         //Fill background
         ctx.fillStyle = "black";
         ctx.fillRect(0, 0, 300, 600);
@@ -26,40 +43,45 @@ function loop(ctx){
                 }
             }
         }
-
         //Fill current block
-        newBlock.blocks.forEach((item, i) => {
-            item.draw(ctx)
-        });
-        //Update current block
-        newBlock.blocks.forEach((item, i) => {
-            item.y += 1;
-            if (item.y === 39){
-                //Block has hit bottom
-                newBlock.blocks.forEach((item, i) => {
-                    settledBlocks[item.x][item.y] = true
-                });
-                console.log(settledBlocks)
-                newBlock = new GameBlock();
-            }
-        });
-    }, 300);
+        ctx.fillStyle = "green"
+        newBlock.blocks.forEach((block, i) => ctx.fillRect(block.x * 15, block.y * 15, 15, 15));
+    }, 100);
 }
 
 class GameBlock {
     constructor() {
-        var ran = Math.floor(Math.random() * 20);
-        this.blocks = [
-            new Block(ran, 0),
-            new Block(ran, 1),
-            new Block(ran, 2),
-            new Block(ran, 3)
-        ]
-    }
-    draw(ctx) {
-        this.blocks.forEach((item, i) => {
-            item.draw();
-        });
+        var ran = Math.floor(Math.random() * 7);
+        switch (ran) {
+            //Line piece
+            case 0:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(10, 2), new Block(10, 3)]
+                break;
+            //T piece
+            case 1:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(10, 2), new Block(11, 1)]
+                break;
+            //Square piece
+            case 2:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(11, 0), new Block(11, 1)]
+                break;
+            //L piece
+            case 3:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(10, 2), new Block(11, 2)]
+                break;
+            //Reverse L piece
+            case 4:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(10, 2), new Block(9, 2)]
+                break;
+            //Squigly piece
+            case 5:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(11, 1), new Block(11, 2)]
+                break;
+            //Reverse squigly piece
+            case 6:
+                this.blocks = [new Block(10, 0), new Block(10, 1), new Block(9, 1), new Block(9, 2)]
+                break;
+        }
     }
 }
 class Block {
@@ -68,8 +90,19 @@ class Block {
         this.y = y;
         this.colour = "red";
     }
-    draw(ctx) {
-        ctx.fillStyle = "red"
-        ctx.fillRect(this.x*15, this.y*15, 15, 15)
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    if (e.keyCode == '38') { }
+    else if (e.keyCode == '40') { }
+    else if (e.keyCode == '37') {
+        //Left arrow key
+        // newBlock.blocks.forEach((block, i) => block.x -= 1);
+    }
+    else if (e.keyCode == '39') {
+        //Right arrow key
+        // newBlock.blocks.forEach((block, i) => block.x += 1);
     }
 }
